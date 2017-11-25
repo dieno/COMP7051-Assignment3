651f9fcf54aeeb48c0370be1341e563e2d3c02e9
Ryan Dieno <ryan.dieno@gmail.com>
2017-11-24 20:29:45 -0800

    Created new scene to work on

--- Assets/Resources/Shaders/MyShader.shader
+++ Assets/Resources/Shaders/MyShader.shader
@@ -19,18 +19,21 @@ Shader "Custom/MyShader" {
         _SpecularShininess("Shininess", Float) = 10
 
         _MainTex("Base (RGB)", 2D) = "white" {}
+		//_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_Color ("Color", Color) = (1,1,1,1)
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
     }
 
     SubShader
     {
-
         Pass
         {
             Tags{ "LightMode" = "ForwardBase" "RenderType"="Opaque"  }
 			LOD 100
 
             CGPROGRAM
-            #pragma target 2.0
+            //#pragma target 2.0
             #pragma vertex vertexShader
             #pragma fragment fragmentShader
             #pragma multi_compile_fwdbase
@@ -146,7 +149,7 @@ Shader "Custom/MyShader" {
                 col.rgb *= lighting;
 
 				UNITY_APPLY_FOG(psIn.fogCoord, col);
-				UNITY_OPAQUE_ALPHA(col.a);
+				//UNITY_OPAQUE_ALPHA(col.a);
 
                 return col;
             }
@@ -183,5 +186,44 @@ Shader "Custom/MyShader" {
             }
             ENDCG
         }
+
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+
+		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+		// #pragma instancing_options assumeuniformscaling
+		UNITY_INSTANCING_CBUFFER_START(Props)
+			// put more per-instance properties here
+		UNITY_INSTANCING_CBUFFER_END
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			// Albedo comes from a texture tinted by color
+			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			o.Albedo = c.rgb;
+			// Metallic and smoothness come from slider variables
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = c.a;
+		}
+		ENDCG
+
     }
 }
--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -9,21 +9,29 @@ public class FirstPersonController : MonoBehaviour {
     public float controllerSensitivity = 5.0f;
     public float UpDownRange = 60.0f;
 
+    public Transform flashlightTransform;
+    public Light flashlightLight;
+
+    public GameObject ballPrefab;
+    public Transform ballSpawn;
+
     private CharacterController cc;
-    private Camera camera;
+    private Camera mCamera;
     private float verticalRotation = 0f;
 
     private Vector3 originalPos;
     private Vector3 originalrot;
     private bool noClip = false;
 
+    private const float BALL_SPEED = 12f;
+
     // Use this for initialization
     void Start () {
         cc = GetComponent<CharacterController>();
-        camera = Camera.main;
+        mCamera = Camera.main;
         originalPos = transform.position;
         originalrot = transform.rotation.eulerAngles;
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 	
 	// Update is called once per frame
@@ -34,7 +42,7 @@ public class FirstPersonController : MonoBehaviour {
         {
             transform.position = originalPos;
             transform.rotation = Quaternion.Euler(originalrot);
-            camera.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            mCamera.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
         }
 
         if(Input.GetKeyDown(KeyCode.W) || Input.GetButtonDown("Triangle"))
@@ -44,19 +52,25 @@ public class FirstPersonController : MonoBehaviour {
 
         Physics.IgnoreLayerCollision(8, 9, noClip);
 
+
         //Rotation
 
         float rotLeftRight = Input.GetAxis("Mouse X") * mouseSesitivity;
         rotLeftRight += Input.GetAxis("RightJoystickX") * controllerSensitivity;
 
         transform.Rotate(0f, rotLeftRight, 0f);
+        
 
         verticalRotation -= Input.GetAxis("Mouse Y") * mouseSesitivity;
         verticalRotation -= Input.GetAxis("RightJoystickY") * controllerSensitivity;
         
         verticalRotation = Mathf.Clamp(verticalRotation, -UpDownRange, UpDownRange);
-        camera.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
-        
+        mCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+
+        flashlightTransform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+
+        //ballSpawn.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+
         //Movement
         float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
         float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
@@ -65,5 +79,31 @@ public class FirstPersonController : MonoBehaviour {
         speed = transform.rotation * speed;
 
         cc.SimpleMove(speed);
-	}
+
+        if (Input.GetKeyDown(KeyCode.Space))// || Input.GetButtonDown("Options"))
+        {
+            ShootBall();
+        }
+
+
+    }
+
+    void ShootBall()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var ball = (GameObject)Instantiate(
+            ballPrefab,
+            ballSpawn.position,
+            ballSpawn.rotation);
+
+        // Add velocity to the bullet
+        ball.GetComponent<Rigidbody>().velocity = ball.transform.forward * BALL_SPEED;
+        Debug.Log("ball: " + ball.transform.forward);
+        Debug.Log("cam: " + mCamera.transform.forward);
+        //ball.GetComponent<Rigidbody>().AddForce(ball.transform.forward * 500);//BALL_SPEED);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(ball, 2.0f);
+    }
+
 }
